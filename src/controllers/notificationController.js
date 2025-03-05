27b2c2768f8e20f8f2c22a0d68ea5f9f874e4bef
FA <fabricecoder009@gmail.com>
2025-03-05 11:18:23 +0200

    feat: added notication processor

---
+++ src/controllers/notificationController.js
@@ -0,0 +1,29 @@
+import { io } from "../app.js";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// Send notification to plumber assigned to a channel
+const sendNotification = async (req, res) => {
+    try {
+        const { channelId, message } = req.body;
+
+        const channel = await prisma.channel.findUnique({
+            where: { id: channelId },
+            include: { plumber: true },
+        });
+        channel.name = undefined;
+
+        if (!channel || !channel.plumber) {
+            return res.status(404).json({ message: "Channel or assigned plumber not found" });
+        }
+
+        io.to(channelId).emit("notification", { message, channel: channel.name });
+
+        res.status(200).json({ message: "Notification sent to plumber", channel: channel.name });
+    } catch (error) {
+        res.status(500).json({ message: "Error sending notification", error });
+    }
+};
+
+export { sendNotification };
